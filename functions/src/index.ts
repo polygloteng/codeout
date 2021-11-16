cd8e1ae35c4cabdfcb4437d9fb335596c07982c7
Kenta Katsumata <polyglot.software@gmail.com>
2021-11-16 16:52:36 +0900

    feat:タスク更新時のトリガー関数を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 import * as admin from 'firebase-admin'
 import { purchase } from './purchase'
 import { mergeHook } from './merge_hook'
+import { onUpdateTask } from './on_update_task'
 
 admin.initializeApp()
 admin.firestore().settings({
   ignoreUndefinedProperties: true,
 })
 
-export { purchase, mergeHook }
+export { purchase, mergeHook, onUpdateTask }
